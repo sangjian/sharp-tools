7eb12417cdba64c6f7b2b20112236410776a9109
sangjian.sj <sangjian.sj@alibaba-inc.com>
2019-07-04 15:54:13 +0800

    增加方法

--- async/src/main/java/cn/ideabuffer/async/core/AsyncTemplate.java
+++ async/src/main/java/cn/ideabuffer/async/core/AsyncTemplate.java
@@ -2,13 +2,18 @@ package cn.ideabuffer.async.core;
 
 import cn.ideabuffer.async.proxy.AsyncProxyUtils;
 import cn.ideabuffer.async.proxy.AsyncResultProxyBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.Callable;
 
 /**
  * @author sangjian.sj
  * @date 2019/06/28
  */
 public class AsyncTemplate {
+
+    private final static Logger logger = LoggerFactory.getLogger(AsyncTemplate.class);
 
     private AsyncExecutor executor;
 
@@ -25,6 +30,36 @@ public class AsyncTemplate {
 
     public void setExecutor(AsyncExecutor executor) {
         this.executor = executor;
+    }
+
+    public <T> T submit(Callable<T> task, Class<T> returnClass) {
+        return submit(task, 0, returnClass);
+    }
+
+    public <T> T submit(Callable<T> task, long timeout, Class<T> returnClass) {
+        return submit(task, timeout, null, returnClass);
+    }
+
+    public <T> T submit(Callable<T> task, long timeout, AsyncCallback<T> callback, Class<T> returnClass) {
+        if(task == null || returnClass == null) {
+            logger.error("task or returnClass is null");
+            throw new IllegalArgumentException("task or returnClass is null");
+        }
+        if(task instanceof AsyncCallable) {
+            return submit((AsyncCallable<T>)task, callback, returnClass);
+        }
+        AsyncCallable<T> callable = new AsyncCallable<T>() {
+            @Override
+            public T call() throws Exception {
+                return task.call();
+            }
+
+            @Override
+            public long getTimeout() {
+                return timeout;
+            }
+        };
+        return submit(callable, callback, returnClass);
     }
 
     public <T> T submit(AsyncCallable<T> task, Class<T> returnClass) {
